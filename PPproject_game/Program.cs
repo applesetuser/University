f549c8db62afee44fbfa2346f23996eec34118f1
Applesetuser <igorkoh2@gmail.com>
2022-01-02 18:55:32 +0100

    porządek na gicie - projekty

---
+++ PPproject_game/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace PPproject_game
+{
+  public class Rand
+  {
+    public int Run(int min, int max)
+    {
+      int range = (max - min) + 1;
+      Random rng = new Random();
+      return min + rng.Next() % range;
+    }
+  }
+
+  public class Hero
+  {
+    public string Name;
+    private int Strength;
+    private int Dexterity;
+    private int Intelligence;
+    public double HP;
+    public double MP;
+
+    private void Init(int strength = 10, int dexterity = 10, int intelligence = 10)
+    {
+      this.Strength = strength;
+      this.Dexterity = dexterity;
+      this.Intelligence = intelligence;
+      HP = 50 + strength;
+      MP = 10 + (3 * intelligence);
+    }
+
+    public int GetStrength() { return this.Strength; }
+    public int GetDexterity() { return this.Dexterity; }
+    public int GetIntelligence() { return this.Intelligence; }
+
+    public void UpStrength() { this.Strength += 5; this.HP += 5; }
+    public void UpDexterity() { this.Dexterity += 5; }
+    public void UpIntelligence() { this.Intelligence += 5; this.MP += (3 * this.Intelligence); }
+
+    public Hero(string name, string myclass)
+    {
+      Name = name;
+      switch(myclass)
+      {
+        case "warior": Init(15, 10, 5); break;
+        case "assassin": Init(5, 15, 10); break;
+        case "sorcerer": Init(5, 5, 20); break;
+        default: Init(); break;
+      }
+    }
+
+    public void Attack(Hero enemy)
+    {
+      Rand rand = new Rand();
+      double damage = Strength * rand.Run(5, 10) / 10;
+
+      if(rand.Run(0, 100) > enemy.GetDexterity())
+      {
+        Console.WriteLine("Bang!");
+        enemy.HP -= damage;
+      }
+      else Console.WriteLine("Dodge!");
+    }
+
+    public void LevelUp()
+    {
+      Console.Write("  1:Strength, 2:Dexterity, 3:Intelligence ... ");
+      int opt = int.Parse(Console.ReadLine());
+
+      switch(opt)
+      {
+        case 1: UpStrength(); break;
+        case 2: UpDexterity(); break;
+        case 3: UpIntelligence(); break;
+      }
+
+      Console.WriteLine();
+    }
+
+    public void Spell()
+    {
+      // TODO: 3 spell's
+    }
+
+    // TODO: Per-round (regeneration...)
+  }
+
+  class Program
+
+  {
+    static void Main(string[] args)
+    {
+      int tour = 1;
+
+      Hero hero1 = new Hero("Edward Białykij", "sorcerer");
+      Console.WriteLine(hero1.Name + " Str:{0} Dex:{1} Int:{2} HP:{3}", hero1.GetStrength(), hero1.GetDexterity(), hero1.GetIntelligence(), hero1.HP);
+
+      Hero hero2 = new Hero("Wataszka Stefan", "assassin");
+      Console.WriteLine(hero2.Name + " Str:{0} Dex:{1} Int:{2} HP:{3}", hero2.GetStrength(), hero2.GetDexterity(), hero2.GetIntelligence(), hero2.HP);
+
+      Console.WriteLine();
+
+      while(hero1.HP > 0 && hero2.HP > 0)
+      {
+        if(tour == 1) Console.WriteLine("Your Turn: " + hero1.Name);
+        else Console.WriteLine("Your Turn: " + hero2.Name);
+
+        Console.Write("1:Attack, 2:Spell, 3:LevelUp ... ");
+        int opt = int.Parse(Console.ReadLine());
+
+        switch(opt)
+        {
+          case 1:
+            if(tour == 1) hero1.Attack(hero2);
+            else hero2.Attack(hero1);
+          break;
+
+          case 2:
+          break;
+
+          case 3:
+            if(tour == 1) hero1.LevelUp();
+            else hero2.LevelUp();
+          break;
+        }
+
+        Console.WriteLine(hero1.Name + " Str:{0} Dex:{1} Int:{2} HP:{3}", hero1.GetStrength(), hero1.GetDexterity(), hero1.GetIntelligence(), hero1.HP);
+        Console.WriteLine(hero2.Name + " Str:{0} Dex:{1} Int:{2} HP:{3}", hero2.GetStrength(), hero2.GetDexterity(), hero2.GetIntelligence(), hero2.HP);
+        Console.WriteLine();
+
+        tour++;
+        if(tour > 2) tour = 1;
+      }
+      // TODO: Win
+    }
+  }
+}
--- PPproject_game/Readme.md
+++
@@ -1,1 +0,0 @@
-Project about simple console game
