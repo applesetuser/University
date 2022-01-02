4f48a63ff09aae19aeab1166439fa72ecf139c4a
Applesetuser <igorkoh2@gmail.com>
2022-01-02 17:55:53 +0100

    First files

---
+++ Baza danych/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Data;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+namespace chmsql
+{
+  class Program
+    {
+        static void CreateBase(string tableName)
+        {
+            string connStr = "server=192.168.64.2;user=root;database=infa;port=8080;password=";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = "CREATE TABLE" + tableName + "(id BIGINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,login VARCHAR(32) UNIQUE NOT NULL,email VARCHAR(64) UNIQUE NOT NULL,password VARCHAR(32) NOT NULL,PRIMARY KEY (id))";
+                
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Utworzono tabelę o nazwie" + tableName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+        }
+        static void AddUser(string tableName, string login, string email, string password)
+        {
+            string connStr = "server=192.168.64.2;user=root;database=infa;port=8080;password=";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = $"INSERT INTO {tableName} (id, login, email, password) VALUES ('null','{login}', '{email}', '{password}')";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+        }
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("Co chcesz zrobic? ");
+                Console.WriteLine();
+                Console.WriteLine("1>> stworzyc baze danych");
+                Console.WriteLine("2>> dodac uzytkownika do bazy danych");
+                Console.WriteLine("3>> exit ");
+                Console.WriteLine();
+                Console.Write("Twój wybór: ");
+                int decision = int.Parse(Console.ReadLine());
+                switch(decision)
+                {
+                    case 1:
+                        Console.Write("Jak chcesz nazwać bazę danych?: ");
+                        string tableName = Console.ReadLine();
+                        CreateBase(tableName);
+                        break;
+                    case 2:
+                        Console.WriteLine("Nazwa bazy danych: ");
+                        tableName = Console.ReadLine();
+
+                        Console.WriteLine("login: ");
+                        string login = Console.ReadLine();
+
+                        Console.WriteLine("email: ");
+                        string email = Console.ReadLine();
+
+                        Console.WriteLine("hasło: ");
+                        string password = Console.ReadLine();
+                        AddUser(tableName, login, email, password);
+                        break;
+                    case 3: break;
+                    default:
+                        Console.WriteLine("wybierz ponownie");
+                        break;
+                }
+
+                if(decision==3) break;
+            }
+        }
+    }
+}
---
+++ Zadania_na_lekcji/hero/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+
+namespace hero
+{
+    public class Hero
+    {
+    public string Name;
+    static int Strength;
+    static int Dexterity;
+    static int Intelligence;
+    static int HP;
+
+    static void Init(int strength = 5, int dexterity = 5, int intelligence = 5, int healthpoints = 100)
+    {
+      Strength = strength;
+      Dexterity = dexterity;
+      Intelligence = intelligence;
+      HP = healthpoints;
+    }
+
+    public int GetStrength() { return Strength; }
+    public int GetDexterity() { return Dexterity; }
+    public int GetIntelligence() { return Intelligence; }
+    public int GetHP() { return HP; }
+
+    public Hero(string name, string myclass)
+      {
+        Name = name;
+        switch(myclass)
+        {
+          case "warior": Init(8, 5, 2, 200); break;
+          case "assassin": Init(3, 8, 4, 100); break;
+          case "sorcerer": Init(4, 2, 9, 100); break;
+          default: Init(); break;
+        }
+      }
+    }
+      /*  public void LevelUp()
+        {
+        if (myclass == warior)
+        {
+            Console.WriteLine("You leveled up !");
+            Console.WriteLine("Strenght +3");
+            Console.WriteLine("Dexterity -1");
+            Console.WriteLine("Intelligence +1");
+            Console.WriteLine("HP +30"); 
+        }
+        else if (myclass == assassin)
+        {
+            Console.WriteLine("You leveled up !");
+            Console.WriteLine("Strenght +3");
+            Console.WriteLine("Dexterity +5");
+            Console.WriteLine("Intelligence +1");
+            Console.WriteLine("HP +10");
+        }
+        else if (myclass == sorcerer)
+        {
+            Console.WriteLine("You leveled up !");
+            Console.WriteLine("Strenght +0");
+            Console.WriteLine("Dexterity +5");
+            Console.WriteLine("Intelligence +10");
+            Console.WriteLine("HP +10");
+        }
+        }  
+        */
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine();
+      Hero hero = new Hero("Edward Białykij", "sorcerer");
+      Console.WriteLine("Heros1: "+ hero.Name + " Str:{0} Dex:{1} Int:{2} HP:{3}", hero.GetStrength(), hero.GetDexterity(), hero.GetIntelligence(), hero.GetHP());
+      Console.WriteLine();
+      Hero hero2 = new Hero("Moczybroda", "warior");
+      Console.WriteLine("Heros2: "+ hero2.Name + " Str:{0} Dex:{1} Int:{2} HP:{3}", hero2.GetStrength(), hero2.GetDexterity(), hero2.GetIntelligence(), hero2.GetHP());
+      Console.WriteLine();
+
+    }
+  }
+}
+
